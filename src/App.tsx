4b66d44bdbc878f1a6505120614fb08562711da4
Norbert Kapuy <norbert.kapuy@gmail.com>
2025-06-22 23:05:34 +0200

    Remove unused import from App.tsx to fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
